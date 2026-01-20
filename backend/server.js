32649517b81f3343ba6a7b94559586d492d87c98
iamshaury <officialshaury@gmail.com>
2026-01-20 21:30:52 +0530

    add port

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import app from "./src/app.js";
 import { config } from "./src/config/env.js";
 
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`)
 })
