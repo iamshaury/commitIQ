19f2ffe9997f02095925411558a427c562ba2c1f
iamshaury <officialshaury@gmail.com>
2026-01-24 10:04:05 +0530

    added uptimerobot

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`)
 })
+
--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,10 @@ import githubRoutes from "./routes/githubRoutes.js";
 const app = express();
 
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
 app.use("/github", githubRoutes);
 
 export default app;
